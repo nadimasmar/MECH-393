c64abbb3f49533cc972e4e35eac189c757e1ad92
ManFromTheMatrix <svaillant04@gmail.com>
2026-02-22 22:35:41 -0500

    Added materials, to reconfig later

--- tables_values.py
+++ tables_values.py
@@ -1,31 +1,112 @@
 nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy = 0.28, 206.8, 80.8, 7800
+props = nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy
 
 steel_carbon = {
     # Standard notation as (Sy, Sut, HB, ν, E, G, ρ)
-    "cold rolled" : {
-        1010 : (303, 365, 105, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        1020 : (393, 469, 131, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        1030 : (441, 524, 149, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        1035 : (462, 552, 163, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        1040 : (490, 586, 170, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        1045 : (531, 627, 179, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        1050 : (579, 689, 197, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
+    # List of different carbon steels as taken from the textbook
+    1010 : {
+        "cold rolled" : (303, 365, 105) + props,
+        "hot rolled" : (179, 324, 95) + props
+    },
+    1020 : {
+        "cold rolled" : (393, 469, 131) + props,
+        "hot rolled" : (207, 379, 111) + props
+    },
+    1030 : {
+        "cold rolled" : (441, 524, 149) + props,
+        "hot rolled" : (259, 469, 137) + props,
+        "normalized" : (345, 517, 149) + props,
+        "tempered 1000" : (517, 669, 255) + props,
+        "tempered 800" : (579, 731, 302) + props,
+        "tempered 400" : (648, 848, 495) + props
+    },
+    1035 : {
+        "cold rolled" : (462, 552, 163) + props,
+        "hot rolled" : (276, 496, 143) + props
+    },
+    1040 : {
+        "cold rolled" : (490, 586, 170) + props,
+        "hot rolled" : (290, 524, 149) + props,
+        "normalized" : (372, 593, 170) + props,
+        "tempered 1200" : (434, 634, 192) + props,
+        "tempered 800" : (552, 758, 241) + props,
+        "tempered 400" : (593, 779, 292) + props
+    },
+    1045 : {
+        "cold rolled" : (531, 627, 179) + props,
+        "hot rolled" : (310, 565, 163) + props,
+    },
+    1050 : {
+        "cold rolled" : (579, 689, 197) + props,
+        "hot rolled" : (579, 689, 197) + props,
+        "normalized" : (427, 745, 217) + props,
+        "tempered 1200" : (538, 717, 235) + props,
+        "tempered 800" : (793, 1089, 444) + props,
+        "tempered 400" : (807, 1124, 514) + props,
+    },
+    1060 : {
+        "hot rolled" : (372, 676, 200) + props,
+        "normalized" : (421, 772, 229) + props,
+        "tempered 1200" : (524, 800, 229) + props,
+        "tempered 1000" : (669, 965, 277) + props,
+        "tempered 800" : (765, 1076, 311) + props,
+    },
+    1095 : {
+        "hot rolled" : (455, 827, 248) + props,
+        "normalized" : (496, 1014, 13) + props,
+        "tempered 1200" : (552, 896, 269) + props,
+        "tempered 800" : (772, 1213, 363) + props,
+        "tempered 600" : (814, 1262, 375) + props
     }
 }
 
 steel_alloy = {
-    # Standard notation as (Sy, Sut, HB, ν, E, G, ρ)
-    "annealed" : {
-        1340 : (434, 703, 204, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        4027 : (324, 517, 150, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        4130 : (359, 558, 156, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        4140 : (421, 655, 197, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        6150 : (407, 662, 192, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-        8740 : (414, 665, 190, nu_steel_alloy, E_steel_alloy, G_steel_alloy, rho_steel_alloy),
-    }
+    # Standard notation as (Sy, Sut, HB, ν, E, G, ρ) ; key is [<number>], and then [<working type>]
+    # List of different alloy steels as taken from the textbook
+    1340 : {
+        "annealed" : (434, 703, 204) + props,
+        "tempered" : (752, 862, 250) + props
+        },
+    4027 : {
+        "annealed" : (324, 517, 150) + props,
+        "tempered" : (779, 910, 264) + props
+        },
+    4130 : {
+        "annealed" : (359, 558, 156) + props,
+        "normalized" : (434, 669, 197) + props,
+        "tempered 1200" : (703, 814, 245) + props,
+        "tempered 800" : (1193, 1282, 380) + props,
+        "tempered 400" : (1462, 1627, 41) + props
+        },
+    4140 : {
+        "annealed" : (421, 655, 197) + props,
+        "normalized" : (655, 1020, 302) + props,
+        "tempered 1200" : (655, 758, 230) + props,
+        "tempered 800" : (1138, 1248, 370) + props,
+        "tempered 400" : (1641, 1772, 510) + props
+        },
+    4340 : {
+        "tempered 1200" : (855, 965, 280) + props,
+        "tempered 1000" : (1076, 1172, 360) + props,
+        "tempered 800" : (1365, 1469, 430) + props,
+        "tempered 600" : (1586, 1724, 486) + props
+        },
+    6150 : {
+        "annealed" : (407, 662, 192) + props,
+        "tempered" : (1020, 1082, 314) + props
+        },
+    8740 : {
+        "annealed" : (414, 665, 190) + props,
+        "tempered" : (917, 993, 288) + props
+        },
 }
 
+# This is a stainless steel that is extremely strong, we could verify the comparison in density to other steels. Values taken when quenched & treated at 600F.
+stainless_steel = (1896, 1965, 57) + props
+
 kt_shoulder_fillet_tension = {
+    # Key is [D/d], yields (a,b) for a * (r/d) ** b
+    # Constants from tables used for approximating the stress concentration factor through a filleted corner in tension
     2.00 : (1.01470, -0.30035),
     1.50 : (0.99957, -0.28221),
     1.30 : (0.99682, -0.25751),
@@ -39,6 +120,8 @@ kt_shoulder_fillet_tension = {
 }
 
 kt_shoulder_fillet_bending = {
+    # Key is [D/d], yields (a,b) for a * (r/d) ** b
+    # Constants from tables used for approximating the stress concentration factor through a filleted corner under a bending moment
     6.00 : (0.87868, -0.33243),
     3.00 : (0.89334, -0.30860),
     2.00 : (0.90879, -0.28598),
@@ -53,6 +136,8 @@ kt_shoulder_fillet_bending = {
 }
 
 kt_shoulder_fillet_torsion = {
+    # Key is [D/d], yields (a,b) for a * (r/d) ** b
+    # Constants from tables used for approximating the stress concentration factor through a filleted corner under an axial torque
     2.00 : (0.86331, -0.23865),
     1.33 : (0.84897, -0.23161),
     1.20 : (0.83425, -0.21649),
@@ -60,6 +145,8 @@ kt_shoulder_fillet_torsion = {
 }
 
 kt_groove_tension = {
+    # Key is [D/d], yields (a,b) for a * (r/d) ** b
+    # Constants from tables used for approximating the stress concentration factor through a groove under tension
     "inf" : (0.99372, -0.39352),
     2.00 : (0.99383, -0.38231),
     1.50 : (0.99808, -0.36955),
@@ -75,6 +162,8 @@ kt_groove_tension = {
 }
 
 kt_groove_bending = {
+    # Key is [D/d], yields (a,b) for a * (r/d) ** b
+    # Constants from tables used for approximating the stress concentration factor through a groove under a bending moment
     "inf" : (0.94801, -0.33302),
     2.00 : (0.93619, -0.33066),
     1.50 : (0.93894, -0.32380),
@@ -91,6 +180,8 @@ kt_groove_bending = {
 }
 
 kt_groove_torsion = {
+    # Key is [D/d], yields (a,b) for a * (r/d) ** b
+    # Constants from tables used for approximating the stress concentration factor through a groove under an axial torque
     "inf" : (0.88126, -0.25204),
     2.00 : (0.89035, -0.24075),
     1.30 : (0.89460, -0.23267),
@@ -102,6 +193,7 @@ kt_groove_torsion = {
 }
 
 neuber_steel = {
+    # Dictionary of neuber constants for steels. Key is [<Sut, in kpsi>], value is sqrt(a)
     50  : 0.130,
     55  : 0.118,
     60  : 0.108,
@@ -119,9 +211,9 @@ neuber_steel = {
     220 : 0.013,
     240 : 0.009
 }
-'''Dictionary of neuber constants for steel'''
 
 neuber_ann_al = {
+    # Dictionary of Neuber constants for annealed aluminum. Key is [<Sut, in kpsi>], value is sqrt(a)
     10 : 0.500,
     15 : 0.341,
     20 : 0.264,
@@ -133,6 +225,7 @@ neuber_ann_al = {
 }
 
 neuber_hard_al = {
+    # Dictionary of Neuber constants for work-hardened aluminum. Key is [<Sut, in kpsi>], value is sqrt(a)
     15 : 0.475,
     20 : 0.380,
     30 : 0.278,
@@ -145,6 +238,7 @@ neuber_hard_al = {
 }
 
 marin_reliability = {
+    # Constants defining C_reliab, or the reliability factor in the corrected endurance limit. Key is [<% reliability>]
     50      : 1,
     90      : 0.897,
     95      : 0.868,
@@ -156,6 +250,8 @@ marin_reliability = {
 }
 
 marin_surface_metric = {
+    # Functions definining the C_surf, or surface factor in the corrected endurance limit.
+    # Key is [<surface finish>], values are (a,b) for the equation a * Sut ** b
     "ground" : (1.58, -0.085),
     "machined" : (4.51, -0.265),
     "cold-rolled" : (4.51, -0.265),
@@ -205,6 +301,7 @@ std_rect_key_range_inch = {
 # https://www.engineersedge.com/gears/shaft_diameter_vs_key_sizes_14411.htm
 
 std_square_key_range_mm = {
+    # Standard cross-sectional dimensions for a square key. Key is [<diameter>], value is width or height of key.
     6   : 2,
     8   : 3,
     10  : 4,
@@ -234,6 +331,7 @@ std_square_key_range_mm = {
 }
 
 std_rect_key_range_mm = {
+    # Standard cross sectional dimensions for a rectangular key. Key is [<diameter>], value is (width, height) of the key
     6   : (2, 2),
     8   : (3, 3),
     10  : (4, 4),
@@ -262,13 +360,16 @@ std_rect_key_range_mm = {
     440 : (100, 50)
 }
 
+'''
 std_wdrf_key_inch = {
     202 : ()
 }
 # to do later
+'''
 
 gear_quality = {
-    # from tables 12-6 and 12-7 in the textbook. Keys are applications or pitch line velocities
+    # Qv, which can determine many gear stress correcting constants.
+    # From tables 12-6 and 12-7 in the textbook. Keys are applications or pitch line velocities, values are tuples defining ranges of Qv.
     "cement mixer" : (3,5),
     "cement kiln" : (5,6),
     "steel mill drive" : (5,6),
@@ -291,7 +392,7 @@ gear_quality = {
 }
 
 face_factor = {
-    # 
+    # Determined by the dimensions of the face. Key is [<gear width>]
     50  : 1.6,
     150 : 1.7,
     250 : 1.8,
@@ -365,6 +466,7 @@ load_distribution_factor = {
 }
 
 application_factor = {
+    # Keys are [<input application>], then [<output application>]
     "uniform" : {
         "uniform"        : 1.00,
         "moderate shock" : 1.25,
